ea69d7a03bf49c3821927f9a0c88eacaf0e53863
Enrico Pelizzon <enrico.pelizzon@gmail.com>
2014-08-09 19:15:15 +0100

    removed mockito's annotations and using lambda-behave's way to create mocks

--- src/test/java/BankSpec.java
+++ src/test/java/BankSpec.java
@@ -1,13 +1,9 @@
-import bank.*;
+import bank.Account;
+import bank.ConversionService;
+import bank.InvalidOperationException;
+import bank.Money;
 import com.insightfullogic.lambdabehave.JunitSuiteRunner;
-import com.insightfullogic.lambdabehave.generators.Generator;
-import com.insightfullogic.lambdabehave.generators.SourceGenerator;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import java.util.ArrayList;
 
 import static bank.Account.account;
 import static bank.Currency.EUR;
@@ -17,64 +13,52 @@ import static com.insightfullogic.lambdabehave.Suite.describe;
 import static org.mockito.Mockito.when;
 
 @RunWith(JunitSuiteRunner.class)
-public class BankSpec {
+public class BankSpec {{
+    describe("a bank account", it -> {
 
-    @Mock
-            private ConversionService conversionService;
-    {
-        describe("a bank account", it -> {
+        final ConversionService conversionService = it.usesMock(ConversionService.class);
+        final Account account = account(GBP, conversionService);
 
-            MockitoAnnotations.initMocks(this);
-//            final ConversionService conversionService = it.usesMock(ConversionService.class);
-            final Account account = account(GBP, conversionService);
+        it.isSetupWith(account::clearBalance);
 
-            it.isSetupWith(account::clearBalance);
+        it.isConcludedWith(account::clearBalance);
 
-            it.isConcludedWith(account::clearBalance);
+        it.should("be empty when created", expect -> {
+            expect.that(account.getBalance()).is(0.0);
+        });
 
-            it.should("be empty when created", expect -> {
-                expect.that(account.getBalance()).is(0.0);
-            });
+        it.should("allow deposits", expect -> {
+            account.deposit(money(1.0, GBP));
+            expect.that(account.getBalance()).is(1.0);
+        });
 
-            it.should("allow deposits", expect -> {
-                account.deposit(money(1.0, GBP));
-                expect.that(account.getBalance()).is(1.0);
-            });
+        it.should("allow payments", expect -> {
+            account.deposit(money(2.0, GBP));
+            account.pay(money(1.0, GBP));
+            expect.that(account.getBalance()).is(1.0);
+        });
 
-            it.should("allow payments", expect -> {
-                account.deposit(money(2.0, GBP));
+        it.should("prevent a payment if the balance can't cover it", expect -> {
+            expect.exception(InvalidOperationException.class, () -> {
                 account.pay(money(1.0, GBP));
-                expect.that(account.getBalance()).is(1.0);
-            });
-
-            it.should("prevent a payment if the balance can't cover it", expect -> {
-                expect.exception(InvalidOperationException.class, () -> {
-                    account.pay(money(1.0, GBP));
-                });
-            });
-
-            it.should("allow deposits in different currencies", expect -> {
-                final Money deposit = money(1.2, EUR);
-                when(conversionService.convert(deposit, GBP)).thenReturn(money(1.0, GBP));
-
-                account.deposit(deposit);
-                expect.that(account.getBalance()).is(1.0);
-            });
-
-            it.requires(10).example(new Generator<Object>() {
-                @Override
-                public Object generate(SourceGenerator source) {
-                    return null;
-                }
-            })
-            it.should("allow payments in different currencies", expect -> {
-                final Money payment = money(1.2, EUR);
-                when(conversionService.convert(payment, GBP)).thenReturn(money(1.0, GBP));
-
-                account.deposit(money(1.0, GBP));
-                account.pay(payment);
-                expect.that(account.getBalance()).is(0.0);
             });
         });
-    }
-}
+
+        it.should("allow deposits in different currencies", expect -> {
+            final Money deposit = money(1.2, EUR);
+            when(conversionService.convert(deposit, GBP)).thenReturn(money(1.0, GBP));
+
+            account.deposit(deposit);
+            expect.that(account.getBalance()).is(1.0);
+        });
+
+        it.should("allow payments in different currencies", expect -> {
+            final Money payment = money(1.2, EUR);
+            when(conversionService.convert(payment, GBP)).thenReturn(money(1.0, GBP));
+
+            account.deposit(money(1.0, GBP));
+            account.pay(payment);
+            expect.that(account.getBalance()).is(0.0);
+        });
+    });
+}}
